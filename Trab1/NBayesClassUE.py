399005509001c9ed3fc14f8ddccee15eb0f700bb
Fuuya <erickasparras@gmail.com>
2023-11-18 11:22:52 +0000

    have to finish fit so i can continue on the predict

--- Trab1/NBayesClassUE.py
+++ Trab1/NBayesClassUE.py
@@ -7,12 +7,45 @@ from sklearn.model_selection import train_test_split
 
 
 class NBayesClassUE:
-    def fit(X,Y):
+    def __init__(self,alpha = 1) -> None:
+        self.alpha = alpha
         
+    def fit(self,X,Y):
+        self.X_train = X
+        self.uniques = np.unique(Y)
+        # Making a temporary dictonary where all unique values have a increasing number from 0
+        values = {value:number for number, value in enumerate(self.uniques)}
+        # Changing Y so it has the numbers instead of names
+        Yfinal = np.vectorize(values.get)(Y)
+        #makes the 2d array into a 1d arrays like the target from sckitlearn
+        self.Y_train = Yfinal.ravel()
         return
     
-    def predict(X):
-        return
+    def score(self,Y):
+        # Verify is the predictions have been made using predict()
+        if self.predictions == None :
+            return -1
+        uniques = np.unique(Y)
+        # Making a temporary dictonary where all unique values have a increasing number from 0
+        values = {value:number for number, value in enumerate(uniques)}
+        # Changing Y so it has the numbers instead of names
+        Y = np.vectorize(values.get)(Y)
+        Y = Y.ravel()
+        pointsright = np.sum(self.predictions == Y ) / len(Y)
+        return pointsright
+
+    def predict(self,X):
+        
+        predictions = [self.predictpriv(t0) for t0 in X]
+        self.predictions = predictions
+        return predictions
     
-    def score(X,Y):
-        return
+
+    def predictpriv(self,X):
+
+
+
+        self.alpha+(X)/(np.unique(self.X_train)*self.alpha+len(self.Y_train))#ESSE Y PRECISA SER ALTERADO PARA CONTAR APENAS O Y USADO AGORA
+
+        pass
+    
--- Trab1/main.py
+++ Trab1/main.py
@@ -13,6 +13,7 @@ cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
 
 X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.5,random_state=1234)
 
+# KNN data
 data0=pd.read_csv('dadosnumericos/iris.csv',header=None,skiprows=1)
 data0X = np.array(data0.iloc[:,:-1].values.tolist())
 data0Y = np.array(data0.iloc[:,-1:].values.tolist())
@@ -38,17 +39,36 @@ Itest0.predict(X_testdf0)
 print("Iris score : ")
 print(Itest0.score(y_testdf0))
 
-Rtest0 = KNeigborsClassUE(k=1,p=1)
-Rtest0.fit(X_traindf1,y_traindf1)
-Rtest0.predict(X_testdf1)
-print("Rice score : ")
-print(Rtest0.score(y_testdf1))
+# Rtest0 = KNeigborsClassUE(k=1,p=1)
+# Rtest0.fit(X_traindf1,y_traindf1)
+# Rtest0.predict(X_testdf1)
+# print("Rice score : ")
+# print(Rtest0.score(y_testdf1))
 
 Wtest0 = KNeigborsClassUE(k=1,p=1)
 Wtest0.fit(X_traindf2,y_traindf2)
 Wtest0.predict(X_testdf2)
 print("Wdbc score : ")
 print(Wtest0.score(y_testdf2))
+
+
+#####################################################################
+# Nbayers data
+data3=pd.read_csv('dadosnominais/bc-nominal.csv',header=None,skiprows=1)
+data3X = np.array(data3.iloc[:,:-1].values.tolist())
+data3Y = np.array(data3.iloc[:,-1:].values.tolist())
+X_traindf3, X_testdf3, y_traindf3, y_testdf3 = train_test_split(data3X,data3Y,test_size=0.5,random_state=1234)
+
+data4=pd.read_csv('dadosnominais/contact-lenses.csv',header=None,skiprows=1)
+data4X = np.array(data4.iloc[:,:-1].values.tolist())
+data4Y = np.array(data4.iloc[:,-1:].values.tolist())
+X_traindf4, X_testdf4, y_traindf4, y_testdf4 = train_test_split(data4X,data4Y,test_size=0.5,random_state=1234)
+
+data5=pd.read_csv('dadosnominais/weather-nominal.csv',header=None,skiprows=1)
+print(data5.head())
+data5X = np.array(data5.iloc[:,:-1].values.tolist())
+data5Y = np.array(data5.iloc[:,-1:].values.tolist())
+X_traindf5, X_testdf5, y_traindf5, y_testd5 = train_test_split(data5X,data5Y,test_size=0.5,random_state=1234)
 
 
 meow = KNeigborsClassUE(k=3,p=1)
